3c5b4c1861a6d151877bcae8b5c2d765cc803c8e
Undefined-user <undefinded-user@users.noreply.github.com>
2021-03-21 16:19:04 +0300

    Validate method. Part 3

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -7,12 +7,11 @@ import {drillProps} from '../functions/drillProps.js'
 const Validator = ({children, ...props}) => {
   // mark for calling method from parent
   const [callFromParent, setCallFromParent] = useState(false)
-  // implicitly drill props to children components
-  const childrenWithProps = drillProps(children, {callFromParent})
   // call child method if parent method was executed
   const useEffect(()=>{
     props.callFromParent&&validate()
   })
+
 
   const validate = () => {
      // if validate method was called at parent component(prevents a cascade reaction)
@@ -22,8 +21,21 @@ const Validator = ({children, ...props}) => {
       setCallFromParent(true)
       return
     }   
-   
+    props.refreshParentCall&&props.refreshParentCall()
   }
+
+  // refresh parent state with little delay 
+  // to prevent updating component during render child component
+  // allows to execute validate method at child as at parent
+  const refreshParentCall = () => {
+    setTimeout(()=>{
+      setCallFromParent(false)
+    }, 10)
+  }
+
+
+  // / implicitly drill props to children components
+  const childrenWithProps = drillProps(children, {callFromParent, refreshParentCall})
 
   return(
     <div>
