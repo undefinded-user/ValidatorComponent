dd531577faaed201b0be2f3c6bfd32d7351a6495
Undefined-user <undefinded-user@users.noreply.github.com>
2021-03-21 16:11:13 +0300

    Correct a typo

--- src/functions/drillProps.js
+++ src/functions/drillProps.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // help function that implisitly drill props to the children
-export const addPropsToChildren = (children, props) => {
+export const drillProps = (children, props) => {
   return React.Children.map(children, (child)=>{
     return React.cloneElement(child, props)
   })
