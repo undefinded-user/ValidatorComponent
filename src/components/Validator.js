50a946fe8694eb6290dba77b751213973a22df0b
Undefined-user <undefinded-user@users.noreply.github.com>
2021-03-21 16:10:09 +0300

    Validate logic. Part 1

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 
-const Validator = ({children}) => {
+import {drillProps} from '../functions/drillProps.js'
+
+
+const Validator = ({children, ...props}) => {
+  // mark for calling method from parent
+  const [callFromParent, setCallFromParent] = useState(false)
+  // implicitly drill props to children components
+  const childrenWithProps = drillProps(children, {callFromParent})
+  // call child method if parent method was executed
+  const useEffect(()=>{
+    props.callFromParent&&validate()
+  })
+
+  const validate = () => {
+
+  }
+
   return(
     <div>
-       {childrend}
+       {childrenWithProps}
     </div>
   )
 }
