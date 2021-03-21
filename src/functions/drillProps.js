0e525fbf7ccfe95734777ffb9c201282c319b69c
Undefined-user <undefinded-user@users.noreply.github.com>
2021-03-21 20:57:04 +0300

    Deploy code

--- src/functions/drillProps.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-// help function that implisitly drill props to the children
-export const drillProps = (children, props) => {
-  return React.Children.map(children, (child)=>{
-    return React.cloneElement(child, props)
-  })
-}
